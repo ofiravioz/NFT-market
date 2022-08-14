a8b909fc515a7cd42a043e1a16bd39d9b9110dc3
Ofir Avioz <ofirav@my.hit.ac.il>
2022-08-14 12:00:02 +0300

    updated

---
+++ nft_market/src/components/ErrorPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import Home from ".Home";
+import PageNotFound from "./404Page";
+
+function ErrorPage() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="*" component={PageNotFound} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default ErrorPage;
