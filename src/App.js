9b90ae463a83d82ce497eebcd21bc19ffbd989f8
Ofir Avioz <ofirav@my.hit.ac.il>
2022-08-16 16:06:58 +0300

    routing

--- src/App.js
+++ src/App.js
@@ -2,48 +2,22 @@ import "./App.css";
 import "./assets/style/main.css";
 import { Button, Container, Stack, Typography } from "@mui/material";
 import { AccordionPanel } from "./components/AccordionPanel";
-import NFTCard from "./components/NFTCard";
-import Email from "./components/Email";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import HeroSection from "./components/HeroSection";
-import appData from "./data/app.json";
+import Home from "./pages/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import PageNotFound from "./components/PageNotFound";
+import PageNotFound from "./pages/PageNotFound";
 function App() {
   return (
     <Container maxWidth="lg">
-      <Header />
-      <Container>
-        <HeroSection data={appData.herotag} />
-      </Container>
-      <Stack direction="row">
-        <AccordionPanel
-          expandedDefault={true}
-          title="History"
-          data={appData.history}
-        />
-        <AccordionPanel
-          expandedDefault={true}
-          title="Bids"
-          data={appData.bids}
-        />
-      </Stack>
-      <Stack className="more-works" direction="row">
-        <Typography gutterBottom variant="h2">
-          More Works by CameronWill{" "}
-        </Typography>
-        <Button color="primary" variant="contained">
-          See All
-        </Button>
-      </Stack>
-      <Stack direction="row" spacing={2}>
-        {appData.nft.map((item) => (
-          <NFTCard key={"nft-item-${item.user.name}"} item={item} />
-        ))}
-      </Stack>
-      <Email />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="*" element={<PageNotFound />}></Route>
+          <Route path="/" index element={<Home />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </Container>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,44 @@
+import "../assets/style/main.css";
+import { Button, Container, Stack, Typography } from "@mui/material";
+import { AccordionPanel } from "../components/AccordionPanel";
+import NFTCard from "../components/NFTCard";
+import Email from "../components/Email";
+import HeroSection from "../components/HeroSection";
+import appData from "../data/app.json";
+function Home() {
+  return (
+    <Container maxWidth="lg">
+      <Container>
+        <HeroSection data={appData.herotag} />
+      </Container>
+      <Stack direction="row">
+        <AccordionPanel
+          expandedDefault={true}
+          title="History"
+          data={appData.history}
+        />
+        <AccordionPanel
+          expandedDefault={true}
+          title="Bids"
+          data={appData.bids}
+        />
+      </Stack>
+      <Stack className="more-works" direction="row">
+        <Typography gutterBottom variant="h2">
+          More Works by CameronWill{" "}
+        </Typography>
+        <Button color="primary" variant="contained">
+          See All
+        </Button>
+      </Stack>
+      <Stack direction="row" spacing={2}>
+        {appData.nft.map((item) => (
+          <NFTCard key={"nft-item-${item.user.name}"} item={item} />
+        ))}
+      </Stack>
+      <Email />
+    </Container>
+  );
+}
+
+export default Home;
